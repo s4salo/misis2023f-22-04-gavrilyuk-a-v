bd74bc042f5640bf8a2aa31e0760dc3000cef33c
s4salo <127080534+s4salo@users.noreply.github.com>
2023-10-24 10:18:22 +0300

    Update threshold.cpp

--- prj.cw/threshold.cpp
+++ prj.cw/threshold.cpp
@@ -1,35 +1,45 @@
 #include<iostream>
-#include<opencv2/opencv.hpp>
 #include<string>
-int main(int argc, const char* argv[]) {
+#include <opencv2/opencv.hpp>
 
-    cv::Mat image = cv::imread("smth2.jpg");
-    std::cout << image.channels();
-    if (!image.data) {
-        std::cout << "Image file not found\n";
+cv::Mat denoiseImage(const cv::Mat& img, int iterations, int hParam, cv::Size kernelSize) {
+
+    cv::Mat result = img;
+    
+    for (int i = 0; i < iterations; i++) {
+        cv::Mat blur;
+        cv::GaussianBlur(result, blur, kernelSize, 0); 
+        
+        cv::Mat temp;
+        cv::fastNlMeansDenoising(blur, temp, hParam);
+        
+        result = temp;
+    }
+
+    return result;
+}
+
+int main() {
+
+    cv::Mat image = cv::imread("smth.jpg");
+
+    if (image.empty()) {
+        std::cout << "Could not read image" << std::endl;
         return 1;
     }
 
-    //Подготовка изображения для нахождения контуров
-    cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
-    cv::threshold(image, image, 128, 255, cv::THRESH_BINARY);
+    cv::Mat gray;
+    cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
 
-    //Нахождение контуров
-    std::vector<std::vector<cv::Point> > contours;
-    cv::Mat contourOutput = image.clone();
-    cv::findContours(contourOutput, contours, cv::RETR_LIST, cv::CHAIN_APPROX_NONE);
+    cv::Mat denoised = denoiseImage(gray, 2, 10, cv::Size(5, 5));
 
-    //Отрисовка контуров
-    cv::Mat contourImage(image.size(), CV_8UC3, cv::Scalar(0, 0, 0)); 
+    // маска
+    cv::Mat mask;
+    cv::threshold(denoised, mask, 128, 255, cv::THRESH_OTSU);  
+    
+    cv::imshow("Image", image);
+    cv::imshow("Mask", mask);
 
-    for (size_t idx = 0; idx < contours.size(); idx++) {
-        cv::drawContours(contourImage, contours, idx, cv::Scalar(255, 0, 0));
-    }
-
-    cv::imshow("Input Image", image);
-    cv::moveWindow("Input Image", 0, 0);
-    cv::imshow("Contours", contourImage);
-    cv::moveWindow("Contours", 200, 0);
     cv::waitKey(0);
 
     return 0;
