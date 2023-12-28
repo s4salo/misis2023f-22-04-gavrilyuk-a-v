41042053fa8025a7ef6c56874a523db0d37e5786
s4salo <gav-ya_030504@mail.ru>
2023-12-28 12:41:36 +0300

    .

--- prj.cw/prj_test.cpp
+++ prj.cw/prj_test.cpp
@@ -23,26 +23,22 @@ int main(int argc, char* argv[]) {
     auto filenames = ImageProcessor::listFiles(inputPicsFolder);
     auto images = ImageProcessor::loadImages(filenames);
 
-    // Process images and create masks using the settings from the configuration file
     auto masks = ImageProcessor::createMasks(images, settings);
 
-    // Create 'results' directory at the same level as 'pics'
+
     fs::path resultsFolder = fs::path(inputPicsFolder).parent_path() / "results";
     if (!fs::exists(resultsFolder)) {
         fs::create_directory(resultsFolder);
     }
 
-    // Save masks to the 'results' folder
     for (int i = 0; i < masks.size(); i++) {
         std::string filename = fs::path(filenames[i]).filename().string();
         cv::Mat& mask = masks[i];
         std::string outputFilename = (resultsFolder / fs::path(filename).stem()).string() + "_mask.png";
         cv::imwrite(outputFilename, mask);
-        // Release the mask since it is no longer needed
         mask.release();
     }
 
-    // Release images that are no longer needed
     for (auto& image : images) {
         image.release();
     }
